989a14f5688326cca8c75eac4c35dc46139c744e
Schmidt-dot <comedian.poliandra@gmail.com>
2026-06-01 22:11:54 +0700

    task3.cpp updated

--- task3.cpp
+++ task3.cpp
@@ -4,14 +4,18 @@
 
 using namespace std;
 
-const int N = 4; //кол-во уравнений и решений
+const int COUNT = 4; //кол-во уравнений и решений
 const double EPS = 0.001; //точность
 
-//ВЫВОД МАТРИЦЫ
-void printMatrix(double matrix[N][N + 1]) {
+const double M = 0.93;
+const double N = 0.07;
+const double P = -0.84;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j <= N; j++) {
+//ВЫВОД МАТРИЦЫ
+void printMatrix(double matrix[COUNT][COUNT + 1]) {
+
+    for (int i = 0; i < COUNT; i++) {
+        for (int j = 0; j <= COUNT; j++) {
             cout << setw(12) << matrix[i][j] << " ";
         }
         cout << endl;
@@ -21,9 +25,9 @@ void printMatrix(double matrix[N][N + 1]) {
 
 
 //Перестановка строк
-void swapStrok(double matrix[N][N + 1], int r1, int r2) { //матрица, строки которые нужно поменять
+void swapStrok(double matrix[COUNT][COUNT + 1], int r1, int r2) { //матрица, строки которые нужно поменять
 
-    for (int j = 0; j <= N; j++) {
+    for (int j = 0; j <= COUNT; j++) {
         double cur = matrix[r1][j];
         matrix[r1][j] = matrix[r2][j];
         matrix[r2][j] = cur;
@@ -34,14 +38,14 @@ void swapStrok(double matrix[N][N + 1], int r1, int r2) { //матрица, ст
 //Метод Гаусса с выбором главного элемента
 void gauss() {
 
-    double matrix[N][N + 1] = { //Расширенная матрица
-        {0.93, -0.04, 0.21, -1.16, -1.24}, // (M)0.93x1 - 0.04x2 + 0.21x3 - 1.16x4 = -1.24
-        {0.25, -1.23, 0.07, -0.09, -0.84}, // 0.25x1 - 1.23x2 + (N)0.07x3 - 0.09x4 = -0.84(P)
-        {-0.21, 0.07, 0.8, -0.13, 2.56}, // -0.21x1 + (N)0.07x2 + 0.8x3 - 0.13x4 = 2.56
-        {0.15, -1.31, 0.06, -0.84, 0.93} // 0.15x1 - 1.31x2 + 0.06x3 - (P)0.84x4 = 0.93(M)
+    double matrix[COUNT][COUNT + 1] = { //Расширенная матрица
+        {M, -0.04, 0.21, -1.16, -1.24}, // (M)0.93x1 - 0.04x2 + 0.21x3 - 1.16x4 = -1.24
+        {0.25, -1.23, N, -0.09, P}, // 0.25x1 - 1.23x2 + (N)0.07x3 - 0.09x4 = -0.84(P)
+        {-0.21, N, 0.8, -0.13, 2.56}, // -0.21x1 + (N)0.07x2 + 0.8x3 - 0.13x4 = 2.56
+        {0.15, -1.31, 0.06, P, M} // 0.15x1 - 1.31x2 + 0.06x3 - (P)0.84x4 = 0.93(M)
     };
 
-    double x[N]; //массив корней
+    double x[COUNT]; //массив корней
 
     cout << "\nМЕТОД ГАУССА\n";
 
@@ -49,12 +53,12 @@ void gauss() {
     printMatrix(matrix);
 
     //Прямой ход (зануляет по диагонали)
-    for (int i = 0; i < N; i++) { 
+    for (int i = 0; i < COUNT; i++) { 
 
         int maxEl = i; //макс эл в столбце
 
         //поиск макс эл
-        for (int k = i + 1; k < N; k++) {
+        for (int k = i + 1; k < COUNT; k++) {
             if (fabs(matrix[k][i]) > fabs(matrix[maxEl][i])) {
                 maxEl = k;
             }
@@ -65,12 +69,12 @@ void gauss() {
         }
 
         //зануляем элементы ниже главной диагонали
-        for (int k = i + 1; k < N; k++) {
+        for (int k = i + 1; k < COUNT; k++) {
 
             double coef = matrix[k][i] / matrix[i][i]; // Aik/Akk
 
             //вычитание строк
-            for (int j = i; j <= N; j++) {
+            for (int j = i; j <= COUNT; j++) {
 
                 matrix[k][j] -= coef * matrix[i][j]; //Bk = Bk - coef * Bi
             }
@@ -82,11 +86,11 @@ void gauss() {
 
 
     // Обратный ход
-    for (int i = N - 1; i >= 0; i--) { //ход с конца
+    for (int i = COUNT - 1; i >= 0; i--) { //ход с конца
 
-        x[i] = matrix[i][N]; //свободный элемент
+        x[i] = matrix[i][COUNT]; //свободный элемент
 
-        for (int j = i + 1; j < N; j++) { //для предыдущих строк
+        for (int j = i + 1; j < COUNT; j++) { //для предыдущих строк
 
             x[i] -= matrix[i][j] * x[j]; 
         }
@@ -96,7 +100,7 @@ void gauss() {
 
     cout << "\nКорни уравнения с помощью метода Гаусса:" << endl;
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < COUNT; i++) {
 
         cout << "x" << i + 1 << " = " << x[i] << endl;
     }
@@ -106,15 +110,15 @@ void gauss() {
 //Метод Зейделя
 void zeidel() {
 
-    double matrix[N][N + 1] = {
-        {0.93, -0.04, 0.21, -1.16, -1.24},
-        {0.25, -1.23, 0.07, -0.09, -0.84},
-        {-0.21, 0.07, 0.8, -0.13, 2.56},
-        {0.15, -1.31, 0.06, -0.84, 0.93}
+    double matrix[COUNT][COUNT + 1] = { //Расширенная матрица
+        {M, -0.04, 0.21, -1.16, -1.24}, 
+        {0.25, -1.23, N, -0.09, P}, 
+        {-0.21, N, 0.8, -0.13, 2.56}, 
+        {0.15, -1.31, 0.06, P, M}
     };
 
-    double x[N] = {0}; //массив корней
-    double oldX[N]; //старые корни до уточнения
+    double x[COUNT] = {0}; //массив корней
+    double oldX[COUNT]; //старые корни до уточнения
 
     double eps;
 
@@ -123,7 +127,7 @@ void zeidel() {
     cout << "МЕТОД ЗЕЙДЕЛЯ:" << endl;
 
     //Таблицы
-    cout << setw(5)  << "N"
+    cout << setw(5)  << "№"
          << setw(12) << "X1"
          << setw(12) << "X2"
          << setw(12) << "X3"
@@ -134,16 +138,16 @@ void zeidel() {
     do {
 
         //сохраняем старые значения
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < COUNT; i++) {
             oldX[i] = x[i];
         }
 
         //вычисление новых x
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < COUNT; i++) {
 
-            double slu = matrix[i][N];
+            double slu = matrix[i][COUNT];
             
-            for (int j = 0; j < N; j++) {
+            for (int j = 0; j < COUNT; j++) {
 
                 if (j != i) {
 
@@ -156,7 +160,7 @@ void zeidel() {
         //Уточнение
         eps = fabs(x[0] - oldX[0]); //новый x - старый x
 
-        for (int i = 1; i < N; i++) {
+        for (int i = 1; i < COUNT; i++) {
 
             if (fabs(x[i] - oldX[i]) > eps) {
                 eps = fabs(x[i] - oldX[i]);
@@ -179,7 +183,7 @@ void zeidel() {
 
     cout << "\nКорни:" << endl;
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < COUNT; i++) {
 
         cout << "x" << i + 1 << " = " << x[i] << endl;
     }
@@ -188,15 +192,15 @@ void zeidel() {
 
 int main() {
 
-    cout << fixed << setprecision(6);
-
 
     cout << "Система уравнений:" << endl;
 
-    cout << "0.93x1 - 0.04x2 + 0.21x3 - 1.16x4 = -1.24" << endl;
-    cout << "0.25x1 - 1.23x2 + 0.07x3 - 0.09x4 = -0.84" << endl;
-    cout << "-0.21x1 + 0.07x2 + 0.8x3 - 0.13x4 = 2.56" << endl;
-    cout << "0.15x1 - 1.31x2 + 0.06x3 - 0.84x4 = 0.93" << endl;
+    cout << M << "x1 - 0.04x2 + 0.21x3 - 1.16x4 = -1.24" << endl;
+    cout << "0.25x1 - 1.23x2 + " << N << "x3 - 0.09x4 = " << P << endl;
+    cout << "-0.21x1 + " << N << "x2 + 0.8x3 - 0.13x4 = 2.56" << endl;
+    cout << "0.15x1 - 1.31x2 + 0.06x3 + " << P << "x4 = " << M << endl;
+
+    cout << fixed << setprecision(6);
 
     gauss();
     cout << endl;
